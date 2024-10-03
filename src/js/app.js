16991707d25158cd38451138dbecd734adcfb4f1
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-10-03 17:19:14 +0300

    Basic layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,31 @@
 import * as siteFunctions from "./modules/functions.js";
-import Swiper, { Navigation, Pagination } from "swiper";
 
 
 siteFunctions.isWebp();
+
+const SECTION_HEADER_SELECTOR = ".js-last-accent";
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    initHeaders();
+
+});
+
+function initHeaders() {
+    console.log('%cjs starts', 'padding: 5px; background: hotpink; color: black;');
+    const sectionHeaders = document.querySelectorAll(SECTION_HEADER_SELECTOR);
+
+    if (sectionHeaders.length) {
+        sectionHeaders.forEach(header => {
+            const text = header.textContent.trim();
+            let words = text.split(' ');
+
+            if (words.length === 0) return;
+
+            let lastWord = words.pop();
+            let newText = words.join(' ') + ' <span class="accent-text">' + lastWord + '</span>';
+            header.innerHTML = newText;
+        });
+    }
+
+}
